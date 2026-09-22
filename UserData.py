f6b8d4e9bd699c569da9377b788da9b1bbd87d05
RoshanJT5 <roshantalreja0501@gmail.com>
2026-09-22 21:41:59 +0530

    first commit

---
+++ UserData.py
@@ -0,0 +1,70 @@
+import sqlite3
+
+def get_connection():
+    conn = sqlite3.connect('Database.db')
+    return conn
+
+#Create Table 
+def create_table():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        '''CREATE TABLE IF NOT EXISTS USERS (
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL
+        )'''
+    )
+    conn.commit()
+    print("Table Created successfully")
+    conn.close()
+
+
+
+#Function to add email,password
+def add_data(username, password):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        INSERT INTO USERS (username, password)
+        VALUES(?,?)
+        ''', (username, password) 
+    )
+    conn.commit()
+    conn.close()
+
+
+
+#Fetch details from table
+def fetch_data():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        SELECT * FROM USERS
+        '''
+    )
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+#Fetch one user details from table
+def find_user(username):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        SELECT * FROM USERS
+        WHERE username = ?
+        ''', (username,)
+    )
+    row = cursor.fetchone()
+    conn.close()
+    return row
+
+
+
+if __name__ == "__main__":
+    create_table()
+    fetch_data()
---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template, redirect, url_for, session, Response, flash
+import UserData
+UserData.create_table()  # Create the table if it doesn't exist
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Change this to a random secret key
+
+@app.route('/' , methods = ['GET' , 'POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form['username']
+        password = request.form['password']
+        user_data = UserData.find_user(user)
+        if user_data:
+            if user_data[1] == user and user_data[2] == password:
+                session['user'] = user
+                return redirect(url_for("dashboard"))
+            else:
+                return render_template("index.html", error = "Invalid Creditials")
+        else:
+            return render_template("index.html", error = "User not found")
+    return render_template("index.html")
+
+@app.route('/dashboard')
+def dashboard():
+    if "user" not in session:
+        return redirect(url_for("login"))        
+    username = session.get("user")
+
+    return render_template("dashboard.html", username = username)
+
+@app.route('/signup', methods = ['GET' , 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        User_data = UserData.find_user(username)
+        if User_data:
+            return render_template("index.html", error = "Username already exists. Choose a different username.")
+        UserData.add_data(username, password)
+        flash("Registration successful!")
+        return redirect(url_for("login"))
+    return render_template("signup.html")
+
+@app.route('/logout', methods = ['POST'])
+def logout():
+    session.pop("user", None)   #session.clear()
+    return redirect(url_for("login"))
+    
+if __name__ == ("__main__"):
+    app.run(debug=True)
